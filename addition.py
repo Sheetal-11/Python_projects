10b5e3d3c19ae173e6e65324adb6324572822ece
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-12 22:22:32 +0530

    Add calculator

---
+++ addition.py
@@ -0,0 +1,15 @@
+# addition.py
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 12 22:01:23 2023
+
+@author: sheet
+"""
+
+def addition():
+    
+    print("Enter two numbers to add:\n")
+    a = int( input("a = "))
+    b = int( input("b = "))
+    
+    return a + b
---
+++ calculator.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 12 22:05:15 2023
+
+@author: sheet
+"""
+
+from addition import *
+#import addition
+
+
